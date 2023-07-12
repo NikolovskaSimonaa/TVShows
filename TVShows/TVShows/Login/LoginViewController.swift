d474c866d9d93aa63bcaeaf518ebb06aba240fb1
NikolovskaSimonaa <nikolovskasimona6@gmail.com>
2023-07-12 16:59:01 +0200

    Better code styling and organisation

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -1,26 +1,17 @@
-//
-//  LoginViewController.swift
-//  TVShows
-//
-//  Created by Infinum Academy 7 on 11.7.23.
-//
-
-//import Foundation
 import UIKit
 
-class LoginViewController:UIViewController{
+final class LoginViewController:UIViewController{
     
-    //@IBOutlet weak var labelCount: UILabel!
     @IBOutlet var labelCount: UILabel!
-    var count:Int=0
+    private var numberOfTaps: Int = 0
     
     override func viewDidLoad(){
         super.viewDidLoad()
         print("Test")
     }
     
-    @IBAction func buttonTouchUpInside(_ sender: Any) {
-        count=count+1
-        labelCount.text = "\(count)"
+    @IBAction private func buttonPressed() {
+        numberOfTaps = numberOfTaps + 1
+        labelCount.text = "\(numberOfTaps)"
     }
 }
