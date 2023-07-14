5641a3e0d659ce3b4d1e0612a4895766f93f505b
NikolovskaSimonaa <nikolovskasimona6@gmail.com>
2023-07-14 13:47:56 +0200

    WIP - Added design to the login screen

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -1,8 +1,43 @@
 import UIKit
-
 final class LoginViewController:UIViewController{
     
-    @IBOutlet var labelCount: UILabel!
+    @IBOutlet var RememberMeButton: UIButton!
+    @IBOutlet var SeePasswordButton: UIButton!
+    @IBOutlet var LoginButton: UIButton!
+    private var RememberMeButtonIsChecked: Bool = false
+    private var SeePasswordButtonIsChecked: Bool = false
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        LoginButton.layer.cornerRadius = 20
+        LoginButton.clipsToBounds = true
+        updateRememberMeIcon()
+    }
+    
+    @IBAction private func RememberMeButtonTapped(_ sender: Any){
+        RememberMeButtonIsChecked.toggle()
+        updateRememberMeIcon()
+    }
+    
+    func updateRememberMeIcon(){
+        let iconName = RememberMeButtonIsChecked ? "checkmark.square" : "square"
+        let icon = UIImage(named: iconName)
+        RememberMeButton.setImage(icon, for: .normal)
+    }
+    
+    @IBAction private func SeePasswordButtonTapped(_ sender: Any) {
+        SeePasswordButtonIsChecked.toggle()
+        updateSeePasswordIcon()
+    }
+    func updateSeePasswordIcon(){
+        let iconName = SeePasswordButtonIsChecked ? "eye" : "eye.slash.fill"
+        let icon = UIImage(named: iconName)
+        SeePasswordButton.setImage(icon, for: .normal)
+    }
+    
+}
+    
+/*    @IBOutlet var labelCount: UILabel!
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     private var numberOfTaps: Int = 0
 
@@ -30,5 +65,5 @@ final class LoginViewController:UIViewController{
     func stopActivityIndicator(){
         activityIndicator.stopAnimating()
     }
-}
+}*/
 
