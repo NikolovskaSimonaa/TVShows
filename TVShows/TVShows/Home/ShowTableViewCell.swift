487dcc2e664e590578718a1dab9141c37a73e9ea
NikolovskaSimonaa <nikolovskasimona6@gmail.com>
2023-08-01 17:22:10 +0200

    Applied changes from home-assignment-06 (manually)

--- TVShows/TVShows/Home/ShowTableViewCell.swift
+++ TVShows/TVShows/Home/ShowTableViewCell.swift
@@ -8,22 +8,9 @@ class ShowTableViewCell: UITableViewCell {
     
     func configure(with show: Show) {
         titleLabel.text = show.title
-        
-        guard let url = URL(string: show.imageUrl!) else {return}
-        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            guard let self = self else { return }
-            let placeholder = UIImage(named: "ic-show-placeholder-vertical")
-            var image:UIImage?
-            do {
-                let imageData = try Data(contentsOf: url)
-                image = UIImage(data: imageData) ?? placeholder!
-            } catch {
-                image = placeholder
-                print("Error loading image: \(error)")
-            }
-            DispatchQueue.main.async {
-                self.iconImageView.image = image
-            }
-        }
+        iconImageView.kf.setImage(
+            with: URL(string: show.imageUrl!),
+            placeholder: UIImage(named: "ic-show-placeholder-vertical")
+        )
     }
 }
